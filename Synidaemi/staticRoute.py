22214aef3d668d669915c0b350ca1e2320f03bae
Gunnar Þórunnarson <gus@tskoli.is>
2021-01-17 20:09:32 +0000

    Create staticRoute.py

---
+++ Synidaemi/staticRoute.py
@@ -0,0 +1,21 @@
+
+# import Flask class in Python
+from flask import Flask
+
+# Create app, that hosts the application
+app = Flask(__name__)
+
+# You can bind more than one route to a single callback (Chaining Decorators)
+@app.route('/')
+@app.route('/page')
+def page():
+    # blöndum saman html elementum og texta í streng sem við skilum.
+    return "<h1>Halló Heimur!</h1>"
+
+# This starts the web app 
+if __name__ == '__main__':
+  app.run(debug=True)  # This will allow the app to display a proper Python error message, so you can fix the typo/syntax error.
+  
+# Keyrðu python skránna og skoðaðu url í vafra
+
+   
