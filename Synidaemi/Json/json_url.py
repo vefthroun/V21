c7f20d8bdfaaeede4b7dc47f25474304d9d973eb
Guðmundur Jón Guðjónsson <gjg1801@gmail.com>
2021-01-29 09:44:51 +0000

    Create json_url.py

---
+++ Synidaemi/Json/json_url.py
@@ -0,0 +1,10 @@
+from flask import Flask, json, render_template
+import urllib.request  # opnar leið að port 80
+
+app = Flask(__name__)
+
+# Tengjumst við internetið - 
+with urllib.request.urlopen("http://apis.is/currency/lb") as url:
+    gogn = json.loads(url.read().decode())
+
+    print(gogn)
