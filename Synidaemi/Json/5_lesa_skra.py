2b3d317a9092b69e71b4058201590bf273d50e0a
Gunnar Þórunnarson <gus@tskoli.is>
2021-01-31 15:41:56 +0000

    Update 5_lesa_skra.py

--- Synidaemi/Json/5_lesa_skra.py
+++ Synidaemi/Json/5_lesa_skra.py
@@ -3,14 +3,14 @@ app = Flask(__name__)
 
 # Tengjumst við skránna 5.json (verður að vera til)
 with open("5.json","r") as skra:
-    gogn = json.load(skra)
+    gogn = json.load(skra) # sækjum data úr Json skrá
 
-# Kíkjum i breytuna gogn
-print(gogn)
+# Prentum nafnið Þórður, gogn = dictionary 
+print(gogn["nemandi"][0]["nafn"])
 
 @app.route('/')
 def index():
-    return gogn
+    return gogn["nemandi"][0]["nafn"] # Þórður
 
-# skoðaðu skrá í vafra
-app.run(debug=True)
+if __name__ == '__main__':
+  app.run(debug=True, use_reloader=True)  
