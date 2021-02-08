d259d45eb544d29f61ff2871096c66713b9280a7
Gunnar Þórunnarson <gus@tskoli.is>
2021-02-08 22:33:24 +0000

    Create 2_cookies.py

---
+++ Synidaemi/Cookies/2_cookies.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, make_response
+app = Flask(__name__)
+
+
+# let's set a cookie with the key of flavor and the value of chocolate chip:
+@app.route("/")
+@app.route("/cookies")
+def cookies():
+
+    # By using make_response() we can build and modify our request ahead of sending it.
+    resp = make_response("Cookies")
+    
+    # búum til cookie og bætum við resp object.
+    # request.path to access the path of the current route
+    # flavor cookie now has an Expires on date along with a value for Path of /cookies.
+    resp.set_cookie(
+        "flavor", 
+        value="chocolate chip",
+        max_age=10,
+        path=request.path
+    )
+
+    # skilar "Cookies" streng
+    return resp
+
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=True)
+
+# https://pythonise.com/series/learning-flask/flask-cookies
+
+"""
+Cookies eru í vaframinni 
+1. Opnaðu Developer tools í vafra og veldu Application (chrome). 
+2. Smelltu á Cookies og skoðaðu upplýsingarnar. 
+3. Hinkraðu í 10 sekúnudur, flavor eyðist þá.
+
+"""
